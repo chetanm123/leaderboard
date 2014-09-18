5c4376a5b7ee57169630c275f590cdab7445f69f
Chetan <chetan@vouchercodesuae.com>
2014-09-18 19:37:27 +0530

    Leaderboard

---
+++ leaderboard.js
@@ -0,0 +1,39 @@
+PlayersList=new Meteor.Collection('players');
+
+if (Meteor.isClient) {
+
+Template.leaderboard.player=function(){
+	return PlayersList.find();
+};
+
+Template.leaderboard.selectedClass=function(){
+	var selectedPlayer = Session.get('selectedPlayer');
+	var playerId = this._id;
+	if(selectedPlayer === playerId)
+		return "selected";
+}
+Template.leaderboard.events({
+	'click li.player':function(){
+		var playerId = this._id;
+		Session.set('selectedPlayer',playerId);
+	}
+});
+
+ /* Template.hello.greeting = function () {
+    return "Welcome to leaderboard.";
+  };
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });*/
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
